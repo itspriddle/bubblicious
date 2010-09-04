4abe10bb0f8daebab0975bc325432e70d4a95b17
Joshua Priddle <jpriddle@nevercraft.net>
2010-09-04 16:46:10 -0400

    Require rails.rb first before other hacks

--- init.rb
+++ init.rb
@@ -1,3 +1,8 @@
-Dir[File.dirname(__FILE__) + "/lib/**/*.rb"].each do |file|
+lib = File.dirname(__FILE__) + '/lib'
+
+# Require lib/rails.rb first, as the others may depend on it
+require "#{lib}/rails.rb"
+
+(Dir["#{lib}/**/*.rb"] - ["#{lib}/rails.rb"]).sort.each do |file|
   require file
 end
