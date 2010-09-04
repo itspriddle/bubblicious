9686b97433043f1c93acbc51c34abde4550422ea
Joshua Priddle <jpriddle@nevercraft.net>
2010-09-04 19:15:48 -0400

    Add Hash#except, move string/object to core_ext

--- init.rb
+++ init.rb
@@ -3,6 +3,6 @@ lib = File.dirname(__FILE__) + '/lib'
 # Require lib/rails.rb first, as the others may depend on it
 require "#{lib}/rails.rb"
 
-(Dir["#{lib}/**/*.rb"] - ["#{lib}/rails.rb"]).sort.each do |file|
+(Dir["#{lib}/**/**.rb"] - ["#{lib}/rails.rb"]).sort.each do |file|
   require file
 end
